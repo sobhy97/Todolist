013aa38cedb57fcfd948965cb13c92ded81b3ec6
sobhy <sobhy6452@gmail.com>
2019-10-01 16:21:02 +0200

    to do list

---
+++ app/src/main/java/com/smartherd/todolist/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.smartherd.todolist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+
+            startActivity(Intent(this,Dashboard::class.java))
+            finish()
+
+        },3000)
+
+    }
+}
